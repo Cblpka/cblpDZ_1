ac8b0e84c68949a168d4c869f3fc1af8d46b7c69
Admin <admin@mac-admin.local>
2017-05-08 10:16:00 +0300

    Initial Commit

---
+++ cblpDZ_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cblpDZ_1
+//
+//  Created by Admin on 08.05.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cblpDZ_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cblpDZ_1
+//
+//  Created by Admin on 08.05.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
